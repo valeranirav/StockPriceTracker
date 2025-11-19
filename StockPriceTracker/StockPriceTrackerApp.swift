be60a9613326c898e7d2cd58e2add1daee85f2a9
Nirav Valera <valera.nirav@gmail.com>
2025-11-19 11:13:29 +0400

    Initial Commit

---
+++ StockPriceTracker/StockPriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPriceTrackerApp.swift
+//  StockPriceTracker
+//
+//  Created by Nirav Valera on 19/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct StockPriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StockPriceTrackerTests/StockPriceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  StockPriceTrackerTests.swift
+//  StockPriceTrackerTests
+//
+//  Created by Nirav Valera on 19/11/2025.
+//
+
+import Testing
+@testable import StockPriceTracker
+
+struct StockPriceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
